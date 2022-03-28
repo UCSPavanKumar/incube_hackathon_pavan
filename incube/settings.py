c7dde4d17280c0d4c4fb583b510e82b35d8c06e2
pavankumar <upadrastapavan@outlook.com>
2022-03-29 01:35:19 +0530

    gave abs path for templates

--- incube/settings.py
+++ incube/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'incube.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'/incube/templates/')],
+        'DIRS': ['/root/incube/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
