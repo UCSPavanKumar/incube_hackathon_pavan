61e3313ae1619e3879b2418fecb89d0df185a6a7
pavankumar <upadrastapavan@outlook.com>
2022-03-29 02:06:43 +0530

    db settings

--- incube/settings.py
+++ incube/settings.py
@@ -78,14 +78,6 @@ WSGI_APPLICATION = 'incube.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-   """ 'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'words',
-        'USER': 'postgres',
-        'PASSWORD':'pavan',
-        'HOST':'localhost',
-        'PORT':'5432'
-    }"""
      'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'dd1o83bdokbkcu',
